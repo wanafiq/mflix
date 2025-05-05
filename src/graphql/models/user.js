856a5736626d120a0f47cf776c8ee0ba28613992
Wan <105415014+wanafiq91@users.noreply.github.com>
2025-05-05 19:09:29 +0800

    (feat) CRUD user

--- src/graphql/models/user.js
+++ src/graphql/models/user.js
@@ -1,48 +1,78 @@
+import { ObjectId } from "mongodb";
+
 export const typeDef = `
   type Query {
-    user: User
+    users(input: Pagination): [User!]!
+    user(id: ID!): User
   }
   
   type Mutation {
-    createUser(user: CreateUserInput!): User
+    createUser(input: CreateUserInput!): User
+    updateUser(id: ID!, input: UpdateUserInput!): User
+    deleteUser(id: ID!): Boolean
   }
   
   input CreateUserInput {
-    name: String
-    age: Int
+    name: String!
+    email: String!
+  }
+  
+  input UpdateUserInput {
+    name: String!
+  }
+  
+  input Pagination {
+    limit: Int
   }
   
   type User {
-    id: Int
+    id: ID!
     name: String
-    age: Int
+    email: String
   }
 `;
 
+// arguments (obj, args, context)
 export const resolvers = {
   Query: {
-    user: () => {
-      return {
-        id: 1,
-        name: "wan",
-        age: 18,
-      };
+    users: async (_, { limit }, { mongo }) => {
+      if (!limit) {
+        limit = 20;
+      }
+
+      return await mongo.users.find().limit(limit).toArray();
+    },
+    user: async (_, { id }, { mongo }) => {
+      return await mongo.users.findOne({
+        _id: new ObjectId(id),
+      });
     },
   },
 
   Mutation: {
-    createUser: async (_, { user }, { mongo }) => {
-      const movies = await mongo.users.find().toArray();
-      console.log(movies);
-
+    createUser: async (_, { input }, { mongo }) => {
+      const response = await mongo.users.insertOne(input);
       return {
-        id: 1,
-        ...user,
+        _id: response.insertedId,
+        ...input,
       };
+    },
+    updateUser: async (_, { id, input }, { mongo }) => {
+      const filter = { _id: new ObjectId(id) };
+      const update = { $set: { name: input.name } };
+      const options = { returnDocument: "after" };
+      return await mongo.users.findOneAndUpdate(filter, update, options);
+    },
+    deleteUser: async (_, { id }, { mongo }) => {
+      const deleted = await mongo.users.deleteOne({ _id: new ObjectId(id) });
+      return deleted.acknowledged === true;
     },
   },
 
   User: {
+    id: ({ _id }) => {
+      return _id; // mongo id is `_id`, we wanted to display as `id`
+    },
     name: (obj) => {
       return obj.name.toUpperCase();
     },
