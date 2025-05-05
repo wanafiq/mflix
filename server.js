efa9e6b70c6ed831304829b6f09b3678f3d3923c
Wan <105415014+wanafiq91@users.noreply.github.com>
2025-05-05 12:34:46 +0800

    (learn) custom resolvers

--- server.js
+++ server.js
@@ -2,36 +2,57 @@ const { graphql, buildSchema } = require("graphql");
 var express = require("express");
 var { createHandler } = require("graphql-http/lib/use/express");
 var { ruruHTML } = require("ruru/server");
+const {
+  GraphQLSchema,
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLInt,
+  GraphQLBoolean,
+} = require("graphql/type");
 
-// Construct a schema, using GraphQL schema language
-const schema = buildSchema(`
-  type Query {
-    hello(name: String!): String
-    age: Int
-    weight: Float
-    isOver18: Boolean
-    hobbies: [String]
-   } 
-`);
+const User = new GraphQLObjectType({
+  name: "User",
+  fields: {
+    id: {
+      type: GraphQLInt,
+    },
+    isAdmin: {
+      type: GraphQLBoolean,
+    },
+    name: {
+      type: GraphQLString,
+      resolve: (obj) => {
+        const name = obj.name.trim().toUpperCase();
+        if (obj.isAdmin) {
+          return `${name} (Admin)`;
+        }
+        return name;
+      },
+    },
+  },
+});
 
-// The rootValue provides a resolver function for each API endpoint
-const rootValue = {
-  hello: ({ name }) => {
-    return "Hello " + name;
-  },
-  age: () => {
-    return 14;
-  },
-  weight: () => {
-    return 64.2;
-  },
-  isOver18: () => {
-    return true;
-  },
-  hobbies: () => {
-    return ["movies, gaming, eating, traveling"];
-  },
-};
+const schema = new GraphQLSchema({
+  query: new GraphQLObjectType({
+    name: "Query",
+    fields: {
+      hello: {
+        type: GraphQLString,
+        resolve: () => "Hello World",
+      },
+      user: {
+        type: User,
+        resolve: () => {
+          return {
+            id: 1,
+            isAdmin: true,
+            name: "Wan",
+          };
+        },
+      },
+    },
+  }),
+});
 
 const app = express();
 
@@ -39,7 +60,6 @@ app.all(
   "/graphql",
   createHandler({
     schema: schema,
-    rootValue: rootValue,
   }),
 );
 
